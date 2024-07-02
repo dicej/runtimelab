277e77a42e23ebd45f5f0304571d9e6b64a3cb1d
Joel Dice <joel.dice@fermyon.com>
2024-07-02 10:30:34 -0600

    update to `wit-bindgen` 0.27.0 and regenerate bindings
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- src/libraries/System.Private.CoreLib/src/System/WASIp2/generate-wasi-poll-bindings.sh
+++ src/libraries/System.Private.CoreLib/src/System/WASIp2/generate-wasi-poll-bindings.sh
@@ -11,11 +11,7 @@ set -ex
 #   [cargo](https://rustup.rs/)
 #   [curl](https://curl.se/download.html)
 
-# TODO: Use a crates.io release instead of the Git repo once a release
-# containing this commit has been created:
-cargo install --locked --no-default-features --features csharp-naot \
-      --git https://github.com/bytecodealliance/wit-bindgen --rev 266d638f7a9c4535ba5fa1f1bb2e8cc6b5d58667 \
-      wit-bindgen-cli
+cargo install --locked --no-default-features --features csharp --version 0.27.0 wit-bindgen-cli
 curl -OL https://github.com/WebAssembly/wasi-http/archive/refs/tags/v0.2.0.tar.gz
 tar xzf v0.2.0.tar.gz
 cat >wasi-http-0.2.0/wit/world.wit <<EOF
@@ -24,4 +20,4 @@ world wasi-poll {
 }
 EOF
 wit-bindgen c-sharp -w wasi-poll -r native-aot --internal --skip-support-files wasi-http-0.2.0/wit
-rm -r wasi-http-0.2.0 v0.2.0.tar.gz WasiHttpWorld_wasm_import_linkage_attribute.cs
+rm -r wasi-http-0.2.0 v0.2.0.tar.gz
--- src/tests/nativeaot/SmokeTests/SharedLibrary/generate-shared-library-bindings.sh
+++ src/tests/nativeaot/SmokeTests/SharedLibrary/generate-shared-library-bindings.sh
@@ -11,10 +11,6 @@ set -ex
 #   [cargo](https://rustup.rs/)
 #   [curl](https://curl.se/download.html)
 
-# TODO: Use a crates.io release instead of the Git repo once a release
-# containing this commit has been created:
-cargo install --locked --no-default-features --features csharp-naot \
-      --git https://github.com/bytecodealliance/wit-bindgen --rev 266d638f7a9c4535ba5fa1f1bb2e8cc6b5d58667 \
-      wit-bindgen-cli
+cargo install --locked --no-default-features --features csharp --version 0.27.0 wit-bindgen-cli
 wit-bindgen c-sharp -w library -r native-aot wit
 rm -r LibraryWorld_wasm_import_linkage_attribute.cs
